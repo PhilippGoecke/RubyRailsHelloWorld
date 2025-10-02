1e2528de1bd2baca07b569f6be201c760222031c
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-10-02 08:00:05 +0200

    Update podman commands for consistency

--- podman-run-rails-graphql.bash
+++ podman-run-rails-graphql.bash
@@ -1,3 +1,3 @@
-podman build --no-cache --rm -f Containerfile.GraphQL -t rails:graphql .
-podman run --interactive --tty -p 3000:3000 rails:graphql
+podman build --no-cache --rm --file Containerfile.GraphQL --tag rails:graphql .
+podman run --interactive --tty --publish 3000:3000 rails:graphql
 echo "browse https://localhost:3000/graphql"
