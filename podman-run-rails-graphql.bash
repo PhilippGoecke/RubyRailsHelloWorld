3aad9f310371fae18190e5554cbb06ffe6d9830e
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-11-01 21:12:01 +0100

    Delete podman-run-rails-graphql.bash

--- podman-run-rails-graphql.bash
+++
@@ -1,3 +0,0 @@
-podman build --no-cache --rm --file Containerfile.GraphQL --tag rails:graphql .
-podman run --interactive --tty --publish 3000:3000 rails:graphql
-echo "browse https://localhost:3000/graphql"
