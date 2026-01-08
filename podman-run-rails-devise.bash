54df6541d2d7a0086757edee72df7ead0b86f360
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-09 00:05:25 +0100

    Fix formatting and update echo message in script

--- podman-run-rails-devise.bash
+++ podman-run-rails-devise.bash
@@ -1,3 +1,3 @@
-podman build --no-cache --rm -f Containerfile.Devise -t rails:devise .
-podman run --interactive --tty -p 3000:3000 rails:devise
-echo "browse https://localhost:3000/?name=test"
+podman build --no-cache --rm --file Containerfile.Devise --tag rails:devise .
+podman run --interactive --tty --publish 3000:3000 rails:devise
+echo "browse https://localhost:3000/?name=Test"
