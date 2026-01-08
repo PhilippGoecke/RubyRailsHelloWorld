ee3fae3e8b7cd3e1639df2401273e31e776b587a
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-09 00:04:51 +0100

    Update podman-run-rails.bash

--- podman-run-rails.bash
+++ podman-run-rails.bash
@@ -1,3 +1,3 @@
-podman build --no-cache --rm -f Containerfile -t rails:demo .
-podman run --interactive --tty -p 3000:3000 rails:demo
+podman build --no-cache --rm --file Containerfile --tag rails:demo .
+podman run --interactive --tty --publish 3000:3000 rails:demo
 echo "browse http://localhost:3000/?name=test"
