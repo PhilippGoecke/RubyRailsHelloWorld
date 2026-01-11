5d61b237d9e88b3eafde40753acb169cec5ad6c8
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-11 20:46:31 +0100

    Change port mapping from 3000 to 3001

--- podman-run-rails-devise.bash
+++ podman-run-rails-devise.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm --file Containerfile.Devise --tag rails:devise .
-podman run --interactive --tty --publish 3000:3000 rails:devise
-echo "browse https://localhost:3000/?name=Test"
+podman run --interactive --tty --publish 3001:3000 rails:devise
+echo "browse https://localhost:3001/?name=Test"
