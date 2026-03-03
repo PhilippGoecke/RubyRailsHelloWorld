0d0482703aed6aa59b90f3f1c5ebaede2772bc20
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-03-03 23:01:04 +0100

    Configure database environment variables for Podman
    Added database environment variables for Podman commands.

--- podman-run-rails-blog.bash
+++ podman-run-rails-blog.bash
@@ -1,3 +1,8 @@
-podman build --no-cache --rm --file Containerfile.Blog --tag rails:blog .
-podman run --interactive --tty --publish 3002:3000 rails:blog
+DB_HOST=localhost
+DB_USERNAME=rails
+DB_PASSWORD=secret
+DB_NAME=blog
+
+podman build --no-cache --rm --file Containerfile.Blog --tag rails:blog --build-arg DB_HOST=$DB_HOST --build-arg DB_USERNAME=$DB_USERNAME --build-arg DB_PASSWORD=$DB_PASSWORD --build-arg DB_NAME=$DB_NAME .
+podman run --interactive --tty --publish 3002:3000 --env DB_HOST=$DB_HOST --env DB_USERNAME=$DB_USERNAME --env DB_PASSWORD=$DB_PASSWORD --env DB_NAME=$DB_NAME rails:blog
 echo "browse https://localhost:3002/"
