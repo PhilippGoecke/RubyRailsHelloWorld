533354b42e1d54c1aafc3fb7abf72c817264e29e
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-04-01 10:03:01 +0200

    Add check for base image before building Devise image

--- podman-run-rails-devise.bash
+++ podman-run-rails-devise.bash
@@ -1,3 +1,10 @@
+if ! podman image ls | grep rails | grep -q base; then
+  echo "Build Base Image ..."
+  podman build --no-cache --rm --file Containerfile.Base --tag rails:base .
+else
+  echo "Base Image present ..."
+fi
+
 podman build --no-cache --rm --file Containerfile.Devise --tag rails:devise .
 podman run --interactive --tty --publish 3001:3000 rails:devise
 echo "browse https://localhost:3001/?name=Test"
