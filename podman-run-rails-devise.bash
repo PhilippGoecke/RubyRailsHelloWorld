8f461310f51caddd57367161434c9587a5982ecd
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-04-14 22:32:47 +0200

    Update podman run command with storage volume
    Create devise_storage directory and update podman run command to use a volume for storage.

--- podman-run-rails-devise.bash
+++ podman-run-rails-devise.bash
@@ -6,5 +6,6 @@ else
 fi
 
 podman build --no-cache --rm --file Containerfile.Devise --tag rails:devise .
-podman run --interactive --tty --publish 3001:3000 rails:devise
+mkdir $(pwd)/devise_storage/
+podman run --interactive --tty --volume $(pwd)/devise_storage/:/rails/demo/storage/ --publish 0.0.0.0:3001:3000 rails:devise
 echo "browse https://localhost:3001/?name=Test"
