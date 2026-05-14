22a91ad45647a8b3be2ad57d8315b0243f4e1003
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-05-14 06:19:37 +0200

    Enhance podman run command with security options

--- podman-run-rails.bash
+++ podman-run-rails.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm --file Containerfile --tag rails:demo .
-podman run --interactive --tty --publish 3000:3000 rails:demo
+podman run --interactive --tty --read-only --cap-drop=ALL --security-opt=no-new-privileges:true --tmpfs /tmp:rw,noexec,nosuid --publish 3000:3000 rails:demo
 echo "browse http://localhost:3000/?name=test"
