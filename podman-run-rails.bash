f127bf73eca9de8e756645b8cdf261db67b648cc
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-05-24 23:09:32 +0200

    Add commands to pull Debian image and inspect it
    Pull the latest Debian image and save its digest.

--- podman-run-rails.bash
+++ podman-run-rails.bash
@@ -1,3 +1,5 @@
+podman pull debian:trixie-slim
+podman inspect --format='{{.RepoDigests}}' debian:trixie-slim >> debian.trixie-slim.sha256
 podman build --no-cache --rm --file Containerfile --tag rails:demo .
 podman run --interactive --tty --read-only --cap-drop=ALL --security-opt=no-new-privileges:true --tmpfs /tmp:rw,noexec,nosuid --publish 3000:3000 rails:demo
 echo "browse http://localhost:3000/?name=test"
