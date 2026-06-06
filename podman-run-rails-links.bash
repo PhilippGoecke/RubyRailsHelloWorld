f43f818c94beb79d85468ff7a7346c600192eeac
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-06-06 21:45:08 +0200

    Update podman-run-rails-links.bash

--- podman-run-rails-links.bash
+++ podman-run-rails-links.bash
@@ -6,5 +6,6 @@ else
 fi
 
 podman build --no-cache --rm --file Containerfile.Links --tag rails:links .
-podman run --interactive --tty --publish 3111:3000 rails:links
+mkdir -p "$(pwd)/links_storage"
+podman run --interactive --tty --publish 3111:3000 --volume $(pwd)/links_storage/:/rails/links/storage/:Z,U rails:links
 echo "browse https://localhost:3111/links"
