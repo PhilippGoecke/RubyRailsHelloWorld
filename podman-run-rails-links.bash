0807117dbb50e00f0afbedcc014c588da21fd835
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-06-28 10:41:57 +0200

    Add volume for certificates in podman run command

--- podman-run-rails-links.bash
+++ podman-run-rails-links.bash
@@ -6,6 +6,6 @@ else
 fi
 
 podman build --no-cache --rm --file Containerfile.Links --tag rails:links .
-mkdir -p "$(pwd)/links_storage"
-podman run --interactive --tty --publish 3111:3000 --volume $(pwd)/links_storage/:/rails/links/storage/:Z,U rails:links
+mkdir -p "$(pwd)/links_storage" "$(pwd)/links_certificates"
+podman run --interactive --tty --publish 3111:3000 --volume $(pwd)/links_storage/:/rails/links/storage/:Z,U --volume $(pwd)/links_certificates/:/rails/links/config/certificates/:Z,U rails:links
 echo "browse https://localhost:3111/links"
