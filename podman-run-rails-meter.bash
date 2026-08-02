9f33ab4f259fa111182855b38bc10fe2f8408309
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-08-02 12:00:47 +0200

    Add storage directory and volume mount for Podman run
    Create a directory for meter storage and mount it in the Podman container.

--- podman-run-rails-meter.bash
+++ podman-run-rails-meter.bash
@@ -1,3 +1,4 @@
 podman build --no-cache --rm --file Containerfile.Meter --tag rails:meter .
-podman run --interactive --tty --read-only --cap-drop=ALL --security-opt=no-new-privileges:true --tmpfs /tmp:rw,noexec,nosuid --pids-limit 256 --memory 512m --cpus 1 --publish 3005:3000 rails:meter
+mkdir -p "$(pwd)/meter_storage"
+podman run --interactive --tty --read-only --cap-drop=ALL --security-opt=no-new-privileges:true --tmpfs /tmp:rw,noexec,nosuid --pids-limit 256 --memory 512m --cpus 1 --publish 3005:3000 --volume $(pwd)/meter_storage/:/rails/demo/storage/:Z,U rails:meter
 echo "browse http://localhost:3005/meters"
