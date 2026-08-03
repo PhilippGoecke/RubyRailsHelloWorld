b7441d3dd5c12dc91e9f286fdce7325112699ce4
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-08-03 12:32:40 +0200

    Update podman-run-rails-tinc.bash

--- podman-run-rails-tinc.bash
+++ podman-run-rails-tinc.bash
@@ -1,4 +1,4 @@
 podman build --no-cache --rm --file Containerfile.Tinc --tag rails:tinc .
 mkdir -p "$(pwd)/tinc_storage"
 podman run --interactive --tty --read-only --cap-drop=ALL --security-opt=no-new-privileges:true --tmpfs /tmp:rw,noexec,nosuid --pids-limit 256 --memory 512m --cpus 1 --publish 3006:3000 --volume $(pwd)/tinc_storage/:/rails/tinc-vpns/storage/:Z,U rails:tinc
-echo "browse http://localhost:3006/"
+echo "browse http://localhost:3006/networks"
